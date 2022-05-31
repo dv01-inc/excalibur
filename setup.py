66b384977bf3c6d74b5c29ea7de26ffdac3dcc17
Albert Yau <albert@dv01.co>
2022-05-31 17:37:27 -0400

    Excalibur Fix

--- setup.py
+++ setup.py
@@ -15,10 +15,10 @@ requires = [
     "camelot-py[cv]>=0.7.1",
     "celery>=4.1.1",
     "Click>=7.0",
-    "configparser>=3.5.0, <3.6.0",
-    "Flask>=1.0.2",
+    "configparser>=5.2.0",
+    "Flask>=2.1.2",
     "SQLAlchemy>=1.2.12",
-    "Werkzeug<1.0.0",
+    "Werkzeug>=2.0",
 ]
 mysql = ["mysqlclient>=1.3.6"]
 all_requires = requires + mysql
